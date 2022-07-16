b266d8c2497f60ea06417fea575edc23a9c22b00
Telmo Viana <telmo.viana@widescope.pt>
2022-07-16 19:15:19 +0100

    project structure and first dependencies and configs

---
+++ adidas-subscription-api/src/main/java/com/adidas/subscription/AdidasSubscriptionApiApplication.java
@@ -0,0 +1,13 @@
+package com.adidas.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdidasSubscriptionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdidasSubscriptionApiApplication.class, args);
+	}
+
+}
---
+++ adidas-subscription-client/src/main/java/com/adidas/subscription/AdidasSubscriptionClientApplication.java
@@ -0,0 +1,13 @@
+package com.adidas.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdidasSubscriptionClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdidasSubscriptionClientApplication.class, args);
+	}
+
+}
