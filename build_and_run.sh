e05f0592e72a4e91b6a7dcde6cef2c2ad6762c47
Telmo Viana <telmo.viana@widescope.pt>
2022-07-22 04:11:33 +0100

    improvements on build and run script

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,12 +4,8 @@ echo 'Adidas Subscription Service Build and Containers run'
 echo 'maven build'
 mvn clean package
 
-echo 'remove containers'
-docker-compose rm -f -s
-
-docker image rm adidas-subscription-api -f
-docker image rm adidas-subscription-mail -f
-docker image rm adidas-subscription-service -f
+echo 'stop containers'
+docker-compose stop
 
 echo 'docker run'
 docker-compose up
